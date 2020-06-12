a1eebbef619f3cc15291ffdf67edc16f3edbdeab
Gabriel Kanegae <gabekanegae@gmail.com>
2020-06-12 19:58:52 -0300

    Day 4! ⭐⭐

---
+++ 04_high_entropy_passphrases.py
@@ -0,0 +1,17 @@
+###########################################
+# --- Day 4: High-Entropy Passphrases --- #
+###########################################
+
+import AOCUtils
+
+###########################################
+
+passphrases = [s.split() for s in AOCUtils.loadInput(4)]
+
+print("Part 1: {}".format(sum(len(p) == len(set(p)) for p in passphrases)))
+
+passphrases = [["".join(sorted(word)) for word in p] for p in passphrases]
+
+print("Part 2: {}".format(sum(len(p) == len(set(p)) for p in passphrases)))
+
+AOCUtils.printTimeTaken()

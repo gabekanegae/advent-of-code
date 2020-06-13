015be0f4b0e6063ed5129778b8bb3a3085959d5b
Gabriel Kanegae <gabekanegae@gmail.com>
2020-06-13 02:53:34 -0300

    Day 3 - Reuse code for both parts

--- 03_spiral_memory.py
+++ 03_spiral_memory.py
@@ -8,44 +8,13 @@ import AOCUtils
 
 square = AOCUtils.loadInput(3)
 
-x, y = 0, 0
-edgeCount = 0
-step = 1
-done = False
-
-while not done:
-    loopStart = True
-
-    for _ in range((edgeCount//4 + 1) * 2):
-        if edgeCount % 4 == 0:
-            if loopStart: # >
-                x += 1
-                loopStart = False
-            else:
-                y -= 1 # ^
-        elif edgeCount % 4 == 1: # <
-            x -= 1
-        elif edgeCount % 4 == 2: # v
-            y += 1
-        elif edgeCount % 4 == 3: # >
-            x += 1
-        
-        step += 1
-        if step == square:
-            distance = abs(x) + abs(y)
-            done = True
-            break
-
-    edgeCount += 1
-
-print("Part 1: {}".format(distance))
+p1, p2 = None, None
 
 x, y = 0, 0
 edgeCount = 0
 seen = {(0, 0): 1}
-done = False
 
-while not done:
+while not (p1 and p2):
     loopStart = True
 
     for _ in range((edgeCount//4 + 1) * 2):
@@ -62,13 +31,18 @@ while not done:
         elif edgeCount % 4 == 3: # >
             x += 1
         
-        seen[(x, y)] = sum(seen[(x+dx, y+dy)] for dx in [-1, 0, 1] for dy in [-1, 0, 1] if (x+dx, y+dy) in seen)
-        if seen[(x, y)] > square:
-            done = True
+        seen[(x, y)] = 0 if p2 else sum(seen[(x+dx, y+dy)] for dx in [-1, 0, 1] for dy in [-1, 0, 1] if (x+dx, y+dy) in seen)
+
+        if not p1 and len(seen) == square:
+            p1 = abs(x) + abs(y)
+        if not p2 and seen[(x, y)] > square:
+            p2 = seen[(x, y)]
+        if p1 and p2:
             break
 
     edgeCount += 1
 
-print("Part 2: {}".format(seen[(x, y)]))
+print("Part 1: {}".format(p1))
+print("Part 2: {}".format(p2))
 
 AOCUtils.printTimeTaken()

9b02342c9481ec4503e1785420d9c68fc68aece9
Gabriel Kanegae <gabekanegae@gmail.com>
2020-10-18 01:59:41 -0300

    Day 21 - Solve Part 2 in O(n)

--- 21_scrambled_letters_and_hash.py
+++ 21_scrambled_letters_and_hash.py
@@ -3,9 +3,8 @@
 ##############################################
 
 import AOCUtils
-from itertools import permutations
 
-def encode(password, steps):
+def scramble(password, steps):
     password = list(password)
     pwdLen = len(password)
 
@@ -25,22 +24,64 @@ def encode(password, steps):
             password[a:b+1] = reversed(password[a:b+1])
         elif step[0] == "rotate":
             if step[1] == "based":
-                c = step[-1]
-                n = password.index(c)
-                n += 1 + int(n >= 4)
+                c = password.index(step[-1])
+                n = c + 1 + int(c >= 4)
             else:
                 n = int(step[2])
-                if step[1] == "left":
-                    n *= -1
-                elif step[1] == "right":
-                    pass
+                if step[1] == "left": n *= -1
 
             n %= pwdLen
-            password = password[-n:] + password[:-n]
+            password = password[-n:] + password[:-n] # Rotate right
         elif step[0] == "swap":
             if step[1] == "letter":
-                ca, cb = step[2], step[5]
-                a, b = password.index(ca), password.index(cb)
+                a, b = password.index(step[2]), password.index(step[5])
+            elif step[1] == "position":
+                a, b = int(step[2]), int(step[5])
+
+            password[a], password[b] = password[b], password[a]
+
+    return "".join(password)
+
+def unscramble(password, steps):
+    password = list(password)
+    pwdLen = len(password)
+
+    for step in reversed(steps):
+        step = step.split()
+        if step[0] == "move":
+            b, a = int(step[2]), int(step[5])
+            c = password[a]
+            if b > a:
+                password[a:b] = password[a+1:b+1]
+            else:
+                password[b+1:a+1] = password[b:a]
+            password[b] = c
+        elif step[0] == "reverse":
+            a, b = int(step[2]), int(step[4])
+
+            password[a:b+1] = reversed(password[a:b+1])
+        elif step[0] == "rotate":
+            if step[1] == "based":
+                c = step[-1]
+
+                # Rotate 1 left until c is at position i
+                # At the 4th rotation, rotate twice
+                i = 0
+                while True:
+                    n = 1 + int(i == 4)
+                    password = password[n:] + password[:n] # Rotate left
+                    if password[i] == c:
+                        break
+                    i += 1
+            else:
+                n = int(step[2])
+                if step[1] == "left": n *= -1
+
+                n %= pwdLen
+                password = password[n:] + password[:n] # Rotate left
+        elif step[0] == "swap":
+            if step[1] == "letter":
+                a, b = password.index(step[2]), password.index(step[5])
             elif step[1] == "position":
                 a, b = int(step[2]), int(step[5])
 
@@ -52,10 +93,8 @@ def encode(password, steps):
 
 steps = AOCUtils.loadInput(21)
 
-print("Part 1: {}".format(encode("abcdefgh", steps)))
+print("Part 1: {}".format(scramble("abcdefgh", steps)))
 
-for password in permutations("abcdefgh"):
-    if encode(password, steps) == "fbgdceah":
-        break
+print("Part 2: {}".format(unscramble("fbgdceah", steps)))
 
-print("Part 2: {}".format("".join(password)))
+AOCUtils.printTimeTaken()

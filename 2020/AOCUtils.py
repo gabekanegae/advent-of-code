ac0cb9828ce59dd21af1e1dc8e48e42ff4113319
Gabriel Kanegae <gabekanegae@gmail.com>
2023-05-29 04:58:33 +0100

    Improve OCR by removing starting/trailing/sequential spaces

--- 2020/AOCUtils.py
+++ 2020/AOCUtils.py
@@ -42,56 +42,84 @@ def print_answer(part, answer):
 
     print(f'Part {part}: {answer}')
 
+################################################################################
+
+alphabets = dict()
+alphabets[6] = ('ABCEFGHIJKLOPRSUYZ',
+    [' ##  ###   ##  #### ####  ##  #  # ###   ## #  # #     ##  ###  ###   ### #  # #   # ####',
+     '#  # #  # #  # #    #    #  # #  #  #     # # #  #    #  # #  # #  # #    #  # #   #    #',
+     '#  # ###  #    ###  ###  #    ####  #     # ##   #    #  # #  # #  # #    #  #  # #    # ',
+     '#### #  # #    #    #    # ## #  #  #     # # #  #    #  # ###  ###   ##  #  #   #    #  ',
+     '#  # #  # #  # #    #    #  # #  #  #  #  # # #  #    #  # #    # #     # #  #   #   #   ',
+     '#  # ###   ##  #### #     ### #  # ###  ##  #  # ####  ##  #    #  # ###   ##    #   ####'])
+alphabets[10] = ('ABCEFGHJKLNPRXZ',
+    ['  ##   #####   ####  ###### ######  ####  #    #    ### #    # #      #    # #####  #####  #    # ######',
+     ' #  #  #    # #    # #      #      #    # #    #     #  #   #  #      ##   # #    # #    # #    #      #',
+     '#    # #    # #      #      #      #      #    #     #  #  #   #      ##   # #    # #    #  #  #       #',
+     '#    # #    # #      #      #      #      #    #     #  # #    #      # #  # #    # #    #  #  #      # ',
+     '#    # #####  #      #####  #####  #      ######     #  ##     #      # #  # #####  #####    ##      #  ',
+     '###### #    # #      #      #      #  ### #    #     #  ##     #      #  # # #      #  #     ##     #   ',
+     '#    # #    # #      #      #      #    # #    #     #  # #    #      #  # # #      #   #   #  #   #    ',
+     '#    # #    # #      #      #      #    # #    # #   #  #  #   #      #   ## #      #   #   #  #  #     ',
+     '#    # #    # #    # #      #      #   ## #    # #   #  #   #  #      #   ## #      #    # #    # #     ',
+     '#    # #####   ####  ###### #       ### # #    #  ###   #    # ###### #    # #      #    # #    # ######'])
+
+def _strip_image(image):
+    image_height, image_width = len(image), len(image[0])
+    empty_columns = [c for c in range(image_width) if not any(image[i][c] for i in range(image_height))]
+
+    # Remove all but one sequential vertical spaces
+    to_remove = set([empty_columns[i] for i in range(1, len(empty_columns)) if empty_columns[i] - empty_columns[i-1] == 1])
+
+    # Strip left and right spaces
+    col = 0
+    while not any(image[i][col] for i in range(image_height)):
+        to_remove.add(col)
+        col += 1
+    col = image_width-1
+    while not any(image[i][col] for i in range(image_height)):
+        to_remove.add(col)
+        col -= 1
+
+    return [[c for i, c in enumerate(l) if i not in to_remove] for l in image]
+
+def _slice_image(image):
+    image = _strip_image(image)
+    
+    image_height, image_width = len(image), len(image[0])
+
+    empty_columns = [c for c in range(image_width) if not any(image[i][c] for i in range(image_height))]
+    unspaced_image = [[c for i, c in enumerate(l) if i not in empty_columns] for l in image]
+
+    empty_columns = [-1] + empty_columns + [image_width]
+    widths = [b-a-1 for a, b in zip(empty_columns, empty_columns[1:])]
+
+    sliced_image = []
+    col = 0
+    for w in widths:
+        sliced_image.append('\n'.join([''.join(['#' if c else ' ' for c in l[col:col+w]]) for l in unspaced_image]))
+        col += w
+
+    return sliced_image
+
 def ocr(image):
-    alphabets = dict()
-    alphabets[6] = ('ABCEFGHIJKLOPRSUYZ',
-        [' ##  ###   ##  #### ####  ##  #  # ###   ## #  # #     ##  ###  ###   ### #  # #   # ####',
-         '#  # #  # #  # #    #    #  # #  #  #     # # #  #    #  # #  # #  # #    #  # #   #    #',
-         '#  # ###  #    ###  ###  #    ####  #     # ##   #    #  # #  # #  # #    #  #  # #    # ',
-         '#### #  # #    #    #    # ## #  #  #     # # #  #    #  # ###  ###   ##  #  #   #    #  ',
-         '#  # #  # #  # #    #    #  # #  #  #  #  # # #  #    #  # #    # #     # #  #   #   #   ',
-         '#  # ###   ##  #### #     ### #  # ###  ##  #  # ####  ##  #    #  # ###   ##    #   ####'])
-    alphabets[10] = ('ABCEFGHJKLNPRXZ',
-        ['  ##   #####   ####  ###### ######  ####  #    #    ### #    # #      #    # #####  #####  #    # ######',
-         ' #  #  #    # #    # #      #      #    # #    #     #  #   #  #      ##   # #    # #    # #    #      #',
-         '#    # #    # #      #      #      #      #    #     #  #  #   #      ##   # #    # #    #  #  #       #',
-         '#    # #    # #      #      #      #      #    #     #  # #    #      # #  # #    # #    #  #  #      # ',
-         '#    # #####  #      #####  #####  #      ######     #  ##     #      # #  # #####  #####    ##      #  ',
-         '###### #    # #      #      #      #  ### #    #     #  ##     #      #  # # #      #  #     ##     #   ',
-         '#    # #    # #      #      #      #    # #    #     #  # #    #      #  # # #      #   #   #  #   #    ',
-         '#    # #    # #      #      #      #    # #    # #   #  #  #   #      #   ## #      #   #   #  #  #     ',
-         '#    # #    # #    # #      #      #   ## #    # #   #  #   #  #      #   ## #      #    # #    # #     ',
-         '#    # #####   ####  ###### #       ### # #    #  ###   #    # ###### #    # #      #    # #    # ######'])
-
-    def slice_image(image):
-        image_height, image_width = len(image), len(image[0])
-
-        empty_columns = [c for c in range(image_width) if not any(image[i][c] for i in range(image_height))]
-        unspaced_image = [[c for i, c in enumerate(l) if i not in empty_columns] for l in image]
-
-        empty_columns = [-1] + empty_columns + [image_width]
-        widths = [b-a-1 for a, b in zip(empty_columns, empty_columns[1:])]
-
-        sliced_image = []
-        r = 0
-        for w in widths:
-            sliced_image.append('\n'.join([''.join(['#' if c else ' ' for c in l[r:r+w]]) for l in unspaced_image]))
-            r += w
-
-        return sliced_image
-
     h = len(image)
     if h not in alphabets:
         raise ValueError(f'Image is of height {h} - only 6 and 10 are supported.')
 
     alphabet_keys, alphabet_values = alphabets[h]
-    alphabet_values = slice_image([[c == '#' for c in l] for l in alphabet_values])
+    alphabet_values = _slice_image([[c == '#' for c in l] for l in alphabet_values])
 
     letters_dict = dict(zip(alphabet_values, alphabet_keys))
 
-    sliced_image = slice_image(image)
+    sliced_image = _slice_image(image)
+
     for c in sliced_image:
         if c not in letters_dict:
             raise ValueError(f'Letter below wasn\'t found in the alphabet of height {h}:\n\n{c}')
 
     return ''.join(letters_dict[c] for c in sliced_image)
+
+if __name__ == '__main__':
+    assert alphabets[6][0] == ocr([[c == '#' for c in l] for l in alphabets[6][1]])
+    assert alphabets[10][0] == ocr([[c == '#' for c in l] for l in alphabets[10][1]])

8200f0221159d4f78e87e39c3a5c08ec5d5b7625
Gabriel Kanegae <gabekanegae@gmail.com>
2023-12-08 06:48:54 +0000

    2023: Day 7 - Simplify function calls

--- 2023/07_camel_cards.py
+++ 2023/07_camel_cards.py
@@ -5,17 +5,20 @@
 import AOCUtils
 from collections import Counter
 
-def cards_sort_key_p1(hand, card_ranks):
+card_rank_p1 = dict(zip(reversed('AKQJT98765432'), range(13)))
+card_rank_p2 = dict(zip(reversed('AKQT98765432J'), range(13)))
+
+def cards_sort_key_p1(hand):
     cards, _ = hand
 
     card_counts = sorted(Counter(cards).values())
 
     type_rank = tuple(sorted(card_counts, reverse=True))
-    card_rank = tuple(card_ranks[c] for c in cards)
+    card_rank = tuple(card_rank_p1[c] for c in cards)
 
     return (type_rank, card_rank)
 
-def cards_sort_key_p2(hand, card_ranks):
+def cards_sort_key_p2(hand):
     cards, _ = hand
 
     if cards == 'JJJJJ':
@@ -26,24 +29,22 @@ def cards_sort_key_p2(hand, card_ranks):
         card_counts[-1] += cards.count('J')
 
     type_rank = tuple(sorted(card_counts, reverse=True))
-    card_rank = tuple(card_ranks[c] for c in cards)
+    card_rank = tuple(card_rank_p2[c] for c in cards)
 
     return (type_rank, card_rank)
 
-def get_total_winnings(cards, alphabet, cards_sort):
-    card_ranks = dict(zip(reversed(alphabet), range(len(alphabet))))
-
-    cards.sort(key=lambda x: cards_sort(x, card_ranks))
-    
-    return sum(rank * int(bid) for rank, (_, bid) in enumerate(cards, start=1))
+def get_total_winnings(hand):
+    return sum(rank * int(bid) for rank, (_, bid) in enumerate(hand, start=1))
 
 ##############################
 
-raw_hand = AOCUtils.load_input(7)
-hand = [l.split() for l in raw_hand]
+raw_cards = AOCUtils.load_input(7)
+cards = [card.split() for card in raw_cards]
 
-AOCUtils.print_answer(1, get_total_winnings(hand, 'AKQJT98765432', cards_sort_key_p1))
+sorted_hand = sorted(cards, key=cards_sort_key_p1)
+AOCUtils.print_answer(1, get_total_winnings(sorted_hand))
 
-AOCUtils.print_answer(2, get_total_winnings(hand, 'AKQT98765432J', cards_sort_key_p2))
+sorted_hand = sorted(cards, key=cards_sort_key_p2)
+AOCUtils.print_answer(2, get_total_winnings(sorted_hand))
 
 AOCUtils.print_time_taken()

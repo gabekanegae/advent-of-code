954760f920b6e37e91b4bd04037ff2dc310be6b5
Gabriel Kanegae <gabekanegae@gmail.com>
2023-12-10 02:09:24 +0000

    2018: Day 17 - Rewrite without recursion

--- 2018/17_reservoir_research.py
+++ 2018/17_reservoir_research.py
@@ -3,101 +3,106 @@
 ######################################
 
 import AOCUtils
-from sys import setrecursionlimit
-setrecursionlimit(5000)
+from collections import defaultdict
 
 class Ground:
-    def __init__(self, x_dim, y_dim):
-        self.x_dim, self.y_dim = x_dim, y_dim
-        self.x_size, self.y_size = x_dim[1]-x_dim[0]+1, y_dim[1]-y_dim[0]+1
-        self.tiles = [['.' for _ in range(self.x_size)] for _ in range(self.y_size)]
+    def __init__(self, clay, source):
+        # For x, water can go up to 1 block to the left/right of existing clay blocks
+        min_x, max_x = min(x for x, _ in clay)-1, max(x for x, _ in clay)+1
+        min_y, max_y = min(y for _, y in clay),   max(y for _, y in clay)
 
-    def set_clay(self, clay):
+        self.bounds = ((min_x, max_x+1), (min_y, max_y+1))
+
+        # Move source down to min_y
+        self.source = (source[0], max(source[1], min_y))
+
+        self.grid = defaultdict(lambda: '.')
         for c in clay:
-            c = (c[0]-self.x_dim[0], c[1]-self.y_dim[0])
-            self.tiles[c[1]][c[0]] = '#'
+            self.grid[c] = '#'
 
-    def get(self, pos): return self.tiles[pos[1]][pos[0]] if self.exists(pos) else None
-    def set(self, pos, data): self.tiles[pos[1]][pos[0]] = data
+    def _is_valid(self, pos):
+        return self.bounds[0][0] <= pos[0] < self.bounds[0][1] and \
+               self.bounds[1][0] <= pos[1] < self.bounds[1][1]
 
-    def exists(self, pos): return 0 <= pos[0] < self.x_size and 0 <= pos[1] < self.y_size
-    def is_barrier(self, pos): return (self.get(pos) == '#' or self.get(pos) == '~')
+    def count(self, c):
+        return list(self.grid.values()).count(c)
 
-    def simulate(self, waterFountain):
-        self._simulate((waterFountain[0]-self.x_dim[0], waterFountain[1]))
+    def simulate(self):
+        stack = [self.source]
+        while stack:
+            cur = stack[-1] # Peek top
+            self.grid[cur] = '|'
 
-    def _simulate(self, pos):
-        self.set(pos, '|')
+            down = (cur[0], cur[1]+1)
+            if self._is_valid(down) and self.grid[down] == '.':
+                stack.append(down)
+            else:
+                stack.pop()
 
-        down = (pos[0], pos[1]+1)
-        left = (pos[0]-1, pos[1])
-        right = (pos[0]+1, pos[1])
+                left_wall = None
+                scout = cur[0]
+                while self._is_valid((scout, cur[1])) and self.grid[(scout, cur[1]+1)] in '~#':
+                    scout -= 1
+                    if self.grid[(scout, cur[1])] in '~#':
+                        left_wall = scout
+                        break
 
-        # If it can still fall, recursive call for down position
-        if self.get(down) == '.':
-            self._simulate(down)
+                right_wall = None
+                scout = cur[0]
+                while self._is_valid((scout, cur[1])) and self.grid[(scout, cur[1]+1)] in '~#':
+                    scout += 1
+                    if self.grid[(scout, cur[1])] in '~#':
+                        right_wall = scout
+                        break
 
-        left_wall = None
-        scout = pos[0]
-        while scout > 0 and self.is_barrier((scout, pos[1]+1)):
-            scout -= 1
-            if self.is_barrier((scout, pos[1])):
-                left_wall = scout
-                break
+                if left_wall and right_wall:
+                    for i in range(left_wall+1, right_wall):
+                        self.grid[(i, cur[1])] = '~'
+                elif self._is_valid(down) and self.grid[down] != '|':
+                    left = (cur[0]-1, cur[1])
+                    if self.grid[left] == '.':
+                        stack.append(left)
+                
+                    right = (cur[0]+1, cur[1])
+                    if self.grid[right] == '.':
+                        stack.append(right)
 
-        right_wall = None
-        scout = pos[0]
-        while scout < self.x_size and self.is_barrier((scout, pos[1]+1)):
-            scout += 1
-            if self.is_barrier((scout, pos[1])):
-                right_wall = scout
-                break
+    def __str__(self):
+        s = []
+        for y in range(*self.bounds[1]):
+            for x in range(*self.bounds[0]):
+                s.append(self.grid[(x, y)])
+            s.append('\n')
 
-        # If position is bound by two walls, it can be made into still water
-        if left_wall and right_wall:
-            for i in range(left_wall+1, right_wall):
-                self.set((i, pos[1]), '~')
-        else: # Otherwise, recursivelly call for left and right positions
-            if self.exists(down) and self.get(down) != '|' and self.get(left) == '.':
-                self._simulate(left)
-
-            if self.exists(down) and self.get(down) != '|' and self.get(right) == '.':
-                self._simulate(right)
-
-    def count_element(self, element):
-        return sum(y.count(element) for y in self.tiles)
+        return ''.join(s)
 
 ######################################
 
 scan = AOCUtils.load_input(17)
-clay = set()
-min_x, max_x, min_y, max_y = None, None, None, None
+source = (500, 0)
 
+clay = set()
 for s in scan:
     s = s.split(', ')
     x = s[0].split('=')[1] # Assume first coordinates are x
     y = s[1].split('=')[1]
     if s[0][0] == 'y': x, y = y, x # If first coordinates are y, swap
 
-    x0, x1 = tuple(map(int, x.split('..'))) if '..' in x else (int(x), int(x))
-    if not min_x or x0 < min_x: min_x = x0
-    if not max_x or x1 > max_x: max_x = x1
+    min_x, max_x = tuple(map(int, x.split('..'))) if '..' in x else (int(x), int(x))
+    min_y, max_y = tuple(map(int, y.split('..'))) if '..' in y else (int(y), int(y))
 
-    y0, y1 = tuple(map(int, y.split('..'))) if '..' in y else (int(y), int(y))
-    if not min_y or y0 < min_y: min_y = y0
-    if not max_y or y1 > max_y: max_y = y1
-
-    for x in range(x0, x1+1):
-        for y in range(y0, y1+1):
+    for x in range(min_x, max_x+1):
+        for y in range(min_y, max_y+1):
             clay.add((x, y))
 
-ground = Ground((min_x-1, max_x+1), (min_y, max_y))
-ground.set_clay(clay)
+ground = Ground(clay, source)
+ground.simulate()
 
-ground.simulate((500, 0))
+# print(ground)
 
-running_water = ground.count_element('|')
-still_water =  ground.count_element('~')
+running_water = ground.count('|')
+still_water = ground.count('~')
+
 AOCUtils.print_answer(1, running_water + still_water)
 AOCUtils.print_answer(2, still_water)
 

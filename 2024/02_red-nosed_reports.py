900f0762fcb33479fe7bf07bbc18a9a4f47a6566
Gabriel Kanegae <gabekanegae@gmail.com>
2024-12-03 01:01:54 +0000

    2024: Day 2 - Improve time complexity from quadratic to linear

--- 2024/02_red-nosed_reports.py
+++ 2024/02_red-nosed_reports.py
@@ -2,25 +2,30 @@
 # --- Day 2: Red-Nosed Reports --- #
 ####################################
 
-def is_report_safe(report):
-    safety_checks = [
-        lambda r: r in [sorted(r), sorted(r, reverse=True)],
-        lambda r: all(1 <= abs(a-b) <= 3 for a, b in zip(r, r[1:]))
-    ]
-
-    return all(is_safe(report) for is_safe in safety_checks)
-
-def is_report_safe_with_tolerance(report):
-    report_variations = [report[:i] + report[i+1:] for i in range(len(report))]
-
-    return any(map(is_report_safe, report_variations))
+import AOCUtils
 
 ####################################
 
-import AOCUtils
+def is_report_safe(report):
+    return any(all((a - b) * tone in [1, 2, 3] for a, b in zip(report, report[1:])) for tone in [1, -1])
+
+def is_report_safe_with_tolerance(report):
+    for tone in [1, -1]:
+        for i in range(len(report)-1):
+            j = i+1
+
+            if (report[j] - report[i]) * tone not in [1, 2, 3]:
+                if is_report_safe(report[:i] + report[i+1:]) or is_report_safe(report[:j] + report[j+1:]):
+                    return True
+                else:
+                    # Check other tone
+                    break
+
+    return False
+
+####################################
 
 raw_reports = AOCUtils.load_input(2)
-
 reports = [list(map(int, r.split())) for r in raw_reports]
 
 AOCUtils.print_answer(1, sum(map(is_report_safe, reports)))

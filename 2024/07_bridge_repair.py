20f76a641180302edc56edcc56bc3e5302f9a2ff
gabekanegae <gabekanegae@gmail.com>
2024-12-08 04:34:13 +0000

    2024: Day 7 - Use BFS, fully prune when partial result is too big

--- 2024/07_bridge_repair.py
+++ 2024/07_bridge_repair.py
@@ -3,32 +3,37 @@
 ################################
 
 import AOCUtils
-from itertools import product
+from collections import deque
 from math import floor, log
 
 ################################
 
-def get_valid_result(equation, operators):
+def get_valid_result(equation, part=1):
     result, values = equation
 
-    for ops in product(operators, repeat=len(values)-1):
-        test_result = values[0]
-        for i, value in enumerate(values[1:]):
-            if ops[i] == '+':
-                test_result += value
-            elif ops[i] == '*':
-                test_result *= value
-            elif ops[i] == '|':
-                test_result = test_result * (10 ** (floor(log(value, 10)) + 1)) + value
+    queue = deque([(1, values[0])])
+    while queue:
+        idx, partial_result = queue.popleft()
 
-            # Since all operators are strictly increasing (for positive operands),
-            # if partial result is larger than expected result, we can break early 
-            if test_result > result:
-                break
+        if idx >= len(values):
+            if partial_result == result:
+                return True, result
+            continue
 
-        if test_result == result:
-            return True, result
+        value = values[idx]
 
+        sum_result = value + partial_result
+        if sum_result <= result:
+            queue.append((idx+1, sum_result))
+
+        mult_result = value * partial_result
+        if mult_result <= result:
+            queue.append((idx+1, mult_result))
+
+        concat_result = partial_result * (10 ** (floor(log(value, 10)) + 1)) + value
+        if part == 2 and concat_result <= result:
+            queue.append((idx+1, concat_result))
+ 
     return False, 0
 
 ################################
@@ -36,13 +41,13 @@ def get_valid_result(equation, operators):
 raw_equations = [list(map(int, l.replace(':', '').split())) for l in AOCUtils.load_input(7)]
 equations = {(l[0], tuple(l[1:])) for l in raw_equations}
 
-results_1 = [get_valid_result(equation, '+*') for equation in equations]
+results_1 = [get_valid_result(equation, part=1) for equation in equations]
 results_sum_1 = sum(result for valid, result in results_1 if valid)
 
 AOCUtils.print_answer(1, results_sum_1)
 
 leftover_equations = [equation for equation, (valid, _) in zip(equations, results_1) if not valid]
-results_2 = results_1 + [get_valid_result(equation, '+*|') for equation in leftover_equations]
+results_2 = results_1 + [get_valid_result(equation, part=2) for equation in leftover_equations]
 results_sum_2 = sum(result for valid, result in results_2 if valid)
 
 AOCUtils.print_answer(2, results_sum_2)
